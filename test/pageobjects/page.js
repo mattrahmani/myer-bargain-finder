cce0c45bc31b9cdbac736c0cf1e7f2a6628a54c8
Matt Rahmani <mmrahmani@yahoo.com>
2021-01-07 16:13:46 +1100

    Update page.js

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -2,6 +2,12 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
+get 
+const username = 'mehdirahmani_1981@yahoo.com';
+const password = 'decvos-4wapji-muSruq';
+
+
+
 module.exports = class Page {
     /**
     * Opens a sub page of the page
@@ -10,5 +16,10 @@ module.exports = class Page {
     open (path) {
         browser.maximizeWindow();
         return browser.url(`https://www.myer.com.au/c/offers/${path}`);
+    }
+
+    login() {
+        browser.url('https://www.myer.com.au/login?continue=%2Faccount&flow=account');
+        
     }
 }
