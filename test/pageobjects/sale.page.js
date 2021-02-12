bb2cffd7f130ccb09dfca81208d04f8273e9a3cc
Matt Rahmani <mmrahmani@yahoo.com>
2021-02-12 12:44:36 +1100

    Screenshots added

--- test/pageobjects/sale.page.js
+++ test/pageobjects/sale.page.js
@@ -2,23 +2,16 @@ const { assert } = require('chai');
 const Page = require('./page');
 const fs = require('fs');
 
-let discountRangeOne = [];
-let discountRangeTwo = [];
-let discountRangeThree = [];
 let items = 0;
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class SalePage extends Page {
 
-    get products() {return $$('li[data-automation=product-grid-item]')}
+    get products() {return $$('li[data-automation=product-grid-item]')};
     get nextBtn() {return $('a=Next') };
-    get pageHeader() {return $('h1.css-1sfahi1')}
-    get pageNumberElement() {return $('ol[data-automation="paginateContainer"] li:nth-child(9)')}
-    get totalProductsElement() {return $('span[data-automation=product-total]')}
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+    get pageHeader() {return $('h1.css-1sfahi1')};
+    get pageNumberElement() {return $('ol[data-automation="paginateContainer"] li:nth-child(9)')};
+    get totalProductsElement() {return $('span[data-automation=product-total]')};
+    get furtherDiscountWrapper() {return $('span*=Take a further')};
+
     open (extension) {
         return super.open(extension);
     }
@@ -42,25 +35,7 @@ class SalePage extends Page {
                 break;
             }
         }
-        // console.log(category + ' Page ' + count);
-        // this.printSaleItems(category);
         assert.equal(totalProducts, items, '=====>>> Some products are missing <<<=====');
-    }
-
-    printSaleItems(category) {
-        console.log('\n=====>>>' + category + ' sale list between 60%-70%:');
-        for (let i=0; i<discountRangeOne.length; i++) {
-            console.log('=====>>> ' + i + ' - ' + discountRangeOne[i]);
-        }
-        // console.log('\n=====>>> ' + category + ' sale list between 70%-80%:');
-        // for (let i=0; i<discountRangeTwo.length; i++) {
-        //     console.log('=====>>> ' + i + ' - ' + discountRangeTwo[i]);
-        // }
-        // console.log('\n=====>>> ' + category + ' sale list over 80%:');
-        // for (let i=0; i<discountRangeThree.length; i++) {
-        //     console.log('=====>>> ' + i + ' - ' + discountRangeThree[i]);
-        // }
-        console.log('Number of Items in ' + category + ' =====>>> ' + items) + '\n';
     }
 
     discountCalculator(category) {
@@ -74,51 +49,56 @@ class SalePage extends Page {
         this.products.forEach(product => {
             items+=1;
             product.waitForDisplayed();
-            product.scrollIntoView();
+            // product.scrollIntoView();
             let productText = product.getText();
             let productHtml = product.getHTML();
-            // console.log(productHtml)
             if (productText.includes('Was') && productText.includes('Now')) {
-                    if (productHtml.includes('brand')) {
-                        productBrand = product.$('span[data-automation=product-brand]').getText();
+                if (productHtml.includes('brand')) {
+                    productBrand = product.$('span[data-automation=product-brand]').getText();
+                }
+                productName = product.$('span[data-automation=product-name]').getText();
+                wasPrice = this.getWasPrice(product);
+                nowPrice = this.getNowPrice(product);
+                if (productText.includes('further')) {
+                    let discount = (this.furtherDiscountWrapper.getText().split(' '))[3];
+                    console.log(discount)
+                    let discountRate = discount.slice(0,2);
+                    console.log('rate: ' + discountRate)
+                    nowPrice = nowPrice - (nowPrice * discountRate/100);
+                }
+                percent = ((1-(nowPrice/wasPrice))*100).toFixed(0);
+                if (60<=percent && percent<70) {
+                    let name = productBrand + ' ' + productName;
+                    // if (productText.includes('further')) {
+                        name = name.split('.').join('').split('/').join('');
+                        filepath = 'screenshots/60to70/' + percent + ' ' + name + '.png';
+                        if (!fs.existsSync(filepath)) {
+                            product.scrollIntoView();
+                            product.$('div[data-automation=product-price]').click();
+                            browser.saveScreenshot(filepath);
+                        }
+                    // }
+                };
+                if (70<=percent && percent<80) {
+                    let name = productBrand + ' ' + productName;
+                    name = name.split('.').join('').split('/').join('');
+                    filepath = 'screenshots/70to80/' + percent + ' ' + name + '.png';
+                    if (!fs.existsSync(filepath)) {
+                        product.scrollIntoView();
+                        product.$('div[data-automation=product-price]').click();
+                        browser.saveScreenshot(filepath);
                     }
-                    productName = product.$('span[data-automation=product-name]').getText();
-                    wasPrice = this.getWasPrice(product);
-                    nowPrice = this.getNowPrice(product);
-                    percent = (1-(nowPrice/wasPrice))*100;
-                    if (50<=percent && percent<70) {
-                        let name = productBrand + ' ' + productName;
-                        // discountRangeOne.push(name);
-                        if (productText.includes('further')) {
-                            name = name.split('.').join('').split('/').join('');
-                            filepath = 'screenshots/60to70/' + name + '.png';
-                            if (!fs.existsSync(filepath)) {
-                                product.$('div[data-automation=product-price]').click();
-                                browser.saveScreenshot(filepath);
-                            }
+                };
+                if (percent>=80) {
+                    let name = productBrand + ' ' + productName;
+                    name = name.split('.').join('').split('/').join('');
+                    filepath = 'screenshots/over80/' + percent + ' ' + name + '.png';
+                    if (!fs.existsSync(filepath)) {
+                        product.scrollIntoView();
+                        product.$('div[data-automation=product-price]').click();
+                        browser.saveScreenshot(filepath);
                         }
-                    };
-                    if (70<=percent && percent<80) {
-                        let name = productBrand + ' ' + productName;
-                        // discountRangeTwo.push(name);
-                        name = name.split('.').join('').split('/').join('');
-                        filepath = 'screenshots/70to80/' + name + '.png';
-                        if (!fs.existsSync(filepath)) {
-                            product.$('div[data-automation=product-price]').click();
-                            browser.saveScreenshot(filepath);
-                        }
-                    };
-                    if (percent>=80) {
-                        let name = productBrand + ' ' + productName;
-                        // discountRangeThree.push(name);
-                        name = name.split('.').join('').split('/').join('');
-                        filepath = 'screenshots/over80/' + name + '.png';
-                        if (!fs.existsSync(filepath)) {
-                            product.$('div[data-automation=product-price]').click();
-                            browser.saveScreenshot(filepath);
-                        }
-                    };
-                // }
+                };
             }
         })
     }
